447550c40b5aa84ce93197f09e645f08b9022780
RoyEde <Roy.ede@gmail.com>
2019-01-13 22:25:50 -0300

    Optimized snackbar code

--- public/snackbar.js
+++ public/snackbar.js
@@ -2,53 +2,54 @@ let showSnackbar;
 
 (function() {
   let snackbarHideTimeOut;
+  let messageTimeout;
+
   const snackbar = document.getElementById('snackbar');
   const snackbarButton = document.getElementById('snackbar-button');
   const snackbarMessage = document.getElementById('snackbar-message');
   const snackbarClassPrefix = tail => `snackbar${tail}`;
 
-  snackbarButton.onclick = () => {
-    clearTimeout(snackbarHideTimeOut);
+  const handleHide = () => {
     snackbar.className = snackbar.className.replace(
       snackbarClassPrefix('--show'),
       snackbarClassPrefix('--hide')
     );
+    messageTimeout = setTimeout(() => {
+      snackbarMessage.textContent = '';
+      clearTimeout(messageTimeout);
+    }, 1000);
+  };
+
+  const handleShow = (message, type) => {
+    snackbar.className = `${snackbarClassPrefix('')} ${snackbarClassPrefix(
+      '--show'
+    )} ${snackbarClassPrefix(`--${type}`)}`;
+    snackbarMessage.textContent = message;
+  };
+
+  snackbarButton.onclick = () => {
+    clearTimeout(snackbarHideTimeOut);
+    handleHide();
   };
 
   // Expose showSnackbar function
-  showSnackbar = (message, type, time = 4000) => {
+  showSnackbar = (message, type = 'deleted', time = 4000) => {
+    clearTimeout(messageTimeout);
     if (snackbar.className.includes(snackbarClassPrefix('--show'))) {
       clearTimeout(snackbarHideTimeOut);
-      snackbar.className = snackbar.className.replace(
-        snackbarClassPrefix('--show'),
-        snackbarClassPrefix('--hide')
-      );
+      handleHide();
       const show = setTimeout(() => {
-        snackbar.className = `${snackbarClassPrefix('')} ${snackbarClassPrefix(
-          '--show'
-        )} ${snackbarClassPrefix(`--${type}`)}`;
-        snackbarMessage.textContent = message;
+        handleShow(message, type);
         snackbarHideTimeOut = setTimeout(() => {
-          snackbar.className = snackbar.className.replace(
-            snackbarClassPrefix('--show'),
-            snackbarClassPrefix('--hide')
-          );
-          snackbarMessage.textContent = '';
+          handleHide();
           clearTimeout(snackbarHideTimeOut);
         }, time);
         clearTimeout(show);
       }, 1000);
     } else {
-      snackbar.className = `${snackbarClassPrefix('')} ${snackbarClassPrefix(
-        '--show'
-      )} ${snackbarClassPrefix(`--${type}`)}`;
-      snackbarMessage.textContent = message;
+      handleShow(message, type);
       snackbarHideTimeOut = setTimeout(() => {
-        snackbar.className = snackbar.className.replace(
-          snackbarClassPrefix('--show'),
-          snackbarClassPrefix('--hide')
-        );
-        snackbarMessage.textContent = '';
+        handleHide();
         clearTimeout(snackbarHideTimeOut);
       }, time);
     }
